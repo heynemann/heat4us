1c8bbf7df9661b21d7b2e9e1c98d3122de27c6b4
Bernardo Heynemann <heynemann@gmail.com>
2011-12-10 10:35:12 -0200

    First app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Heat4us::Application.config.secret_token = '97c09f210dc94b5a134732d965cb9d1e5c65242885acb23e5d4712d61c7124db5d624b5e531901f756136b446cbe9c4bc1e8b3d237b67e19c216962a36e4bebb'
